764de04c6a482a5af521076409bbb12f99dd4f57
AleksandrsVorkuls <a.vorkuls@mail.ru>
2022-03-31 18:07:17 +0300

    task2

---
+++ task_2_1.py
@@ -0,0 +1,25 @@
+nums = []
+sums_1 = 0
+sums_2 = 0
+for i in range(1, 1001):
+    if i % 2 != 0:
+        nums.append(i ** 3)
+
+for idx in range(len(nums)):
+    num_sum = 0
+    j = nums[idx]
+    while j:
+        num_sum += j % 10
+        j = j // 10
+    if num_sum % 7 == 0:
+        sums_1 += nums[idx]
+    nums[idx]  += 17
+    num_sum = 0
+    j = nums[idx]
+    while j:
+        num_sum += j % 10
+        j = j // 10
+    if num_sum % 7 == 0:
+        sums_2 += nums[idx]
+print(sums_1)
+print(sums_2)
